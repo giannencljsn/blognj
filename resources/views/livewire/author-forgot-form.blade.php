ca3fea88593d94afe3bbf9c2671446242ff0fd29
giannencljsn <giannejuson16@gmail.com>
2024-10-25 01:32:17 +0800

    +mail SMTP configuration success

--- resources/views/livewire/author-forgot-form.blade.php
+++ resources/views/livewire/author-forgot-form.blade.php
@@ -1,7 +1,13 @@
 <div>
+    @if(Session::get('fail'))
+        <div class="alert alert-danger">
+            {!! Session::get('fail') !!}
+        </div>
+    @endif
+
     @if(Session::get('success'))
         <div class="alert alert-success">
-
+            {!! Session::get('success') !!}
         </div>
     @endif
 
