7c5e56e15f234fd3a73777cb085f459108cf9849
giannencljsn <giannejuson16@gmail.com>
2024-11-09 04:52:11 +0800

    centered logo

--- resources/views/front/layouts/inc/header.blade.php
+++ resources/views/front/layouts/inc/header.blade.php
@@ -1,44 +1,52 @@
 <header class="navigation">
     <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light px-0">
-            <a class="navbar-brand order-1 py-0" href="/">
-                <img loading="prelaod" decoding="async" class="img-fluid" src="{{ blogInfo()->blog_logo }}"
-                    alt="{{ blogInfo()->blog_name }}" style="max-width: 100px">
-            </a>
-            <div class="navbar-actions order-3 ml-0 ml-md-4">
-                <button aria-label="navbar toggler" class="navbar-toggler border-0" type="button"
-                    data-toggle="collapse" data-target="#navigation"> <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
-            <form action="{{ route('search_posts') }}" class="search order-lg-3 order-md-2 order-3 ml-auto">
-                <input id="search-query" name="query" value="{{ Request('query') }}" type="search" placeholder="Search..." autocomplete="off">
-            </form>
-            <div class="collapse navbar-collapse text-center order-lg-2 order-4" id="navigation">
-                <ul class="navbar-nav mx-auto mt-3 mt-lg-0">
-                    <li class="nav-item"> <a class="nav-link" href="about.html">About Me</a>
-                    </li>
-                    @foreach( \App\Models\Category::whereHas('subcategories', function($q){
-                        $q->whereHas('posts');
-                    })->orderBy('ordering','asc')->get() as $category )
-                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {{ $category->category_name }}
-                        </a>
-                        <div class="dropdown-menu"> 
-                            @foreach( \App\Models\SubCategory::where('parent_category',$category->id)->whereHas('posts')->orderby('ordering','asc')->get() as $subcategory )
-                            <a class="dropdown-item" href="{{ route('category_posts',$subcategory->slug) }}">{{ $subcategory->subcategory_name }}</a>
-                            @endforeach
+        <nav class="navbar navbar-expand-lg navbar-light px-0 justify-content-center">
+            <div class="collapse navbar-collapse text-center" id="navigation">
+                <ul class="navbar-nav mt-3 mt-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="about.html">About Me</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">App dev.</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="http://blognj.lcl/category/flutter">Flutter</a>
+                            <a class="dropdown-item" href="http://blognj.lcl/category/kotlin">Kotlin</a>
                         </div>
                     </li>
-                    @endforeach
-                    @foreach( \App\Models\SubCategory::where('parent_category',0)->whereHas('posts')->orderBy('ordering','asc')->get() as $subcategory )
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('category_posts',$subcategory->slug) }}">{{ $subcategory->subcategory_name }}</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">Web dev.</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="http://blognj.lcl/category/codeigniter">CodeIgniter</a>
+                            <a class="dropdown-item" href="http://blognj.lcl/category/cakephp">CakePHP</a>
+                            <a class="dropdown-item" href="http://blognj.lcl/category/laravel">Laravel</a>
+                        </div>
                     </li>
-                    @endforeach
-                    <li class="nav-item"> <a class="nav-link" href="contact.html">Contact</a>
-                    </li>
+                    <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
                 </ul>
+
+
             </div>
+            <!-- Right-aligned and centered logo -->
+            <div class="centerit d-flex justify-content-center align-items-center ml-auto">
+                <a class="navbar-brand py-0" href="/">
+                    <img loading="preload" decoding="async" class="img-fluid"
+                        src="http://blognj.lcl/back/dist/img/logo-favicon/1730643078_22304_larablog_logo.png"
+                        alt="BLOGNJ" style="max-width: 100px">
+                </a>
+            </div>
+
+            <div class="navbar-actions">
+                <button aria-label="navbar toggler" class="navbar-toggler border-0" type="button" data-toggle="collapse"
+                    data-target="#navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <form action="http://blognj.lcl/search" class="search ml-auto">
+                    <input id="search-query" name="query" value="codeigniter" type="search" placeholder="Search..."
+                        autocomplete="off">
+                </form>
+            </div>
+
         </nav>
+
     </div>
 </header>
