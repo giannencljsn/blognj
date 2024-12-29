a20f2bd051cf30c4dc2dcf0100e7563ac490295d
giannencljsn <giannejuson16@gmail.com>
2024-12-30 03:51:21 +0800

    +dynamic url for header links

--- resources/views/front/layouts/inc/header.blade.php
+++ resources/views/front/layouts/inc/header.blade.php
@@ -8,17 +8,17 @@
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
                             aria-haspopup="true" aria-expanded="false">App dev.</a>
                         <div class="dropdown-menu">
-                            <a class="dropdown-item" href="http://blognj.lcl/category/flutter">Flutter</a>
-                            <a class="dropdown-item" href="http://blognj.lcl/category/kotlin">Kotlin</a>
+                            <a class="dropdown-item" href="{{ url('category/flutter') }}">Flutter</a>
+                            <a class="dropdown-item" href="{{ url('category/kotlin') }}">Kotlin</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
                             aria-haspopup="true" aria-expanded="false">Web dev.</a>
                         <div class="dropdown-menu">
-                            <a class="dropdown-item" href="http://blognj.lcl/category/codeigniter">CodeIgniter</a>
-                            <a class="dropdown-item" href="http://blognj.lcl/category/cakephp">CakePHP</a>
-                            <a class="dropdown-item" href="http://blognj.lcl/category/laravel">Laravel</a>
+                            <a class="dropdown-item" href="{{ url('category/codeigniter') }}">CodeIgniter</a>
+                            <a class="dropdown-item" href="{{ url('category/cakephp') }}">CakePHP</a>
+                            <a class="dropdown-item" href="{{ url('category/laravel') }}">Laravel</a>
                         </div>
                     </li>
                     <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
@@ -29,8 +29,7 @@
             <!-- Right-aligned and centered logo -->
             <div class="centerit d-flex justify-content-center align-items-center ml-auto">
                 <a class="navbar-brand py-0" href="/">
-                    <img loading="preload" decoding="async" class="img-fluid"
-                        src="http://blognj.lcl/back/dist/img/logo-favicon/1730643078_22304_larablog_logo.png"
+                    <img loading="preload" decoding="async" class="img-fluid" src="{{ blogInfo()->blog_logo }}"
                         alt="BLOGNJ" style="max-width: 100px">
                 </a>
             </div>
